67bb87f66d712dcd0504645fddcc597b031c29c8
meggolbek <mgolbek@eng.ucsd.edu>
2018-02-05 13:07:57 -0800

    routing to places screen added

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,22 @@
  */
 
 exports.view = function(req, res){
-    res.render('index');
+    res.render('index', {
+        'row1': [
+            {'place': 'ATM',
+              'id': 'index-atm-button'
+            },
+            {'place': 'Gas Station',
+                'id': 'index-gas-button'
+            }
+        ],
+        'row2': [
+            {'place': 'Bar',
+                'id': 'index-bar-button'
+            },
+            {'place': 'Restaurant',
+                'id': 'index-restaurant-button'
+            }
+        ]
+    });
 };
