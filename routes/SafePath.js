5e76465023506cf99d4fd2ce57450cda0fe2d591
meggolbek <mgolbek@eng.ucsd.edu>
2018-02-10 13:20:49 -0800

    un commented google api

--- routes/SafePath.js
+++ routes/SafePath.js
@@ -1,7 +1,6 @@
 exports.view = function(req, res){
 	var start = req.body.start;
     var destination = req.body.destination;
-    console.log("post received: %s %s COUCOU" , start, destination);
     res.render('safePath', {
         'start': start,
         'destination' : destination
