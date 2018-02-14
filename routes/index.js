85d7f5db28d3bc01f3f2bf8836f6d25e865e39ff
meggolbek <mgolbek@eng.ucsd.edu>
2018-02-13 23:07:04 -0800

    added safety tip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 /*
  * GET home page.
  */
-
 exports.view = function(req, res){
+    var data = require("../safetyTips.json");
+    var random = Math.floor((Math.random()*16));
+    var tip = data.tips[random];
+    console.log(tip);
     res.render('index', {
+        'tip':tip,
         'row1': [
             {'place': 'ATM',
               'id': 'index-atm-button'
