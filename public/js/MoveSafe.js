24b739195bdf881ea08bca716c9ee6bb64dd6f14
meggolbek <mgolbek@eng.ucsd.edu>
2018-03-01 16:13:13 -0800

    fixing

--- public/js/MoveSafe.js
+++ public/js/MoveSafe.js
@@ -10,7 +10,9 @@
  */
 function initializePage() {
     // add any functionality and listeners you want here
+    console.log("here1");
     $(".testButton").click(function(e){
+        console.log("here2");
         gtag('event', 'click', {
             'event_category': 'testBtn',
             'event_label': 'user clicked tester',
