d81ee45419947fbf3c89c0e95f51fe96d651a1c9
meggolbek <mgolbek@eng.ucsd.edu>
2018-03-01 16:19:50 -0800

    fixing

--- public/js/MoveSafe.js
+++ public/js/MoveSafe.js
@@ -12,13 +12,13 @@ function initializePage() {
     // add any functionality and listeners you want here
     console.log("here1");
     $(".testButton").click(function(e){
+        e.preventDefault();
         console.log("here2");
         gtag('event', 'click', {
             'event_category': 'testBtn',
             'event_label': 'user clicked tester',
             'value': 1
         });
-        window.location.href="/routing/"+ document.getElementById('origin-input-path').value+"/"+document.getElementById('destination-input-path').value;
     });
 }
 
